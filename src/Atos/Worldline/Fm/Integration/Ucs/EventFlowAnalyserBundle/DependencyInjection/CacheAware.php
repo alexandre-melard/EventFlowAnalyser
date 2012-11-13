a21085d4d34dd3c2acf27a3f60aee31fa7f913df
amelard <alexandre@melard.fr>
2012-11-13 23:07:33 +0100

    modified cache and added multiple file upload craft

---
+++ src/Atos/Worldline/Fm/Integration/Ucs/EventFlowAnalyserBundle/DependencyInjection/CacheAware.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: A140980
+ * Date: 12/11/12
+ * Time: 23:14
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Atos\Worldline\Fm\Integration\Ucs\EventFlowAnalyserBundle\DependencyInjection;
+
+use Doctrine\Common\Cache\Cache;
+
+class CacheAware
+{
+    protected $cache;
+
+    public function __construct(Cache $c)
+    {
+        $this->cache = $c;
+    }
+
+    public function setCache($cache)
+    {
+        $this->cache = $cache;
+    }
+
+}
